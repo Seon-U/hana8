83b0b12885f83978cc497600544a559cbc52bcdc
Seon-U <seonu.kim.kr@gmail.com>
2025-12-19 16:20:44 +0900

    add storyBook

--- react/rbvite/src/components/ui/btn.tsx
+++ react/rbvite/src/components/ui/btn.tsx
@@ -1,8 +1,10 @@
+import { cn } from '@/lib/utils';
 import type { PropsWithChildren } from 'react';
 
 type Prop = {
   onClick?: () => void;
   type?: 'reset' | 'submit';
+  variant?: 'default' | 'destructive' | 'primary';
   disabled?: boolean;
   className?: string;
 };
@@ -12,12 +14,19 @@ export default function Btn({
   type,
   className,
   disabled,
+  variant = 'default',
   children,
 }: PropsWithChildren<Prop>) {
   return (
     <button
       type={type}
-      className={`${className}`}
+      className={cn(
+        {
+          'bg-red-500 text-white': variant === 'destructive',
+          'bg-primary-foreground': variant === 'primary',
+        },
+        className
+      )}
       onClick={onClick}
       disabled={disabled}
     >
