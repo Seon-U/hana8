297eb2daba60cfe348b938f0cb03aff33578444d
Seon-U <seonu.kim.kr@gmail.com>
2026-02-05 13:55:20 +0900

    test example T

--- java/hello/src/main/java/com/hana8/hello/trythis/StringLogParser.java
+++ java/hello/src/main/java/com/hana8/hello/trythis/StringLogParser.java
@@ -1,7 +1,5 @@
 package com.hana8.hello.trythis;
 
-import java.util.Arrays;
-
 public class StringLogParser {
 	public static void main(String[] args) {
 		String log = """
@@ -11,42 +9,65 @@ public class StringLogParser {
 			2024-02-05 09:18:33 WARN UserService: Password retry limit reached for user john
 			2024-02-05 09:19:01 ERROR UserService: Login failed for user admin
 			2024-02-05 09:20:15 INFO OrderService: New order created #1235""";
-		String[] loglist = log.split("\n");
-		String[] service = new String[loglist.length];
-		String[] adminLog = new String[loglist.length];
-		String[] errorLog = new String[loglist.length];
 
-		int errorNumber = 0;
-		int infoNumber = 0;
-		int warnNumber = 0;
-		for (int i=0; i< loglist.length ;i++) {
-			String[] tokens = loglist[i].trim().split("\\s+");
-			String type = tokens[2];
-			if (type.contains("ERROR")) {
-				errorNumber += 1;
-				errorLog[i] = loglist[i];
-			} else if (type.contains("INFO")) {
-				infoNumber += 1;
-			} else if (type.contains("WARN")) {
-				warnNumber += 1;
+		int errorCnt = 0;
+		int infoCnt = 0;
+		int warnCnt = 0;
+		final String[] services = {"UserService", "PaymentService", "DatabaseService", "OrderService"};
+		int[] serviceCnts = {0, 0, 0, 0};
+
+		StringBuilder sbAdmin = new StringBuilder();
+		StringBuilder sbError = new StringBuilder();
+		String[] logs = log.split("\n");
+		for (String l : logs) {
+			// String[] arr = l.split("Service:");
+			String[] dt_tm_lvl_svc_msg = l.split(" ", 5);
+			// System.out.println("arr = " + Arrays.toString(dt_tm_lvl_svc_msg));
+
+			String lvl = dt_tm_lvl_svc_msg[2];
+			String svc = dt_tm_lvl_svc_msg[3].replace(":", "");
+			String msg = dt_tm_lvl_svc_msg[4];
+			switch (lvl) {
+				case "ERROR" -> errorCnt++;
+				case "INFO" -> infoCnt++;
+				case "WARN" -> warnCnt++;
 			}
-			service[i] = tokens[3];
-			if (loglist[i].contains("admin")) {
-				adminLog[i] = loglist[i];
+
+			// IntStream.range(0, services.length).filter(i -> services[i].equals(svc)).findFirst().orElse(-1)
+			// Arrays.asList(services).indexOf(svc)
+			for (int i = 0; i < services.length; i++) {
+				if (services[i].equals(svc))
+					serviceCnts[i]++;
+			}
+
+			if (msg.contains("admin")) {
+				if (!sbAdmin.isEmpty())
+					sbAdmin.append('\n');
+				sbAdmin.append(l);
+			}
+
+			if (lvl.equals("ERROR")) {
+				if (!sbError.isEmpty())
+					sbError.append('\n');
+				sbError.append(svc).append(':').append(' ').append(msg);
+			}
+		} // end of for...
+
+		System.out.printf("1. 전체 로그: %d개%n", logs.length);
+		System.out.printf("2. ERROR: %d개, INFO: %d개, WARN: %d개%n", errorCnt, infoCnt, warnCnt);
+
+		int bigServiceCnt = 0;
+		String bigService = "";
+		for (int i = 0; i < serviceCnts.length; i++) {
+			if (bigServiceCnt < serviceCnts[i]) {
+				bigServiceCnt = serviceCnts[i];
+				bigService = services[i];
 			}
 		}
+		System.out.printf("3. 최다 등장한 서비스: %s (%d회)%n", bigService, bigServiceCnt);
+		// System.out.println("serviceCnts = " + Arrays.toString(serviceCnts));
 
-		for (String s : service) {
-
-		}
-
-		System.out.println("전체로그 수: " + loglist.length + "개");
-		System.out.printf("ERROR: %d개, INFO: %d개, WARN: %d개", errorNumber, infoNumber, warnNumber);
-		System.out.printf("최다 등장한 서비스: %s (%d회)");
-		System.out.println("admin 관련 로그");
-		System.out.println(Arrays.toString(adminLog));
-		System.out.println("ERROR 로그 모음");
-		System.out.println(Arrays.toString(errorLog));
-
+		System.out.println("4. admin 관련 로그\n" + sbAdmin);
+		System.out.println("5. Error 로그\n" + sbError);
 	}
 }
