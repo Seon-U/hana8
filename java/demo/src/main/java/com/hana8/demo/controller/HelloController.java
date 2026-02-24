a7f700302a00c46e2dc22d3107278632e22d4ae9
Seon-U <seonu.kim.kr@gmail.com>
2026-02-24 15:40:58 +0900

    learn layered archi Spring

--- java/demo/src/main/java/com/hana8/demo/controller/HelloController.java
+++ java/demo/src/main/java/com/hana8/demo/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.hana8.demo;
+package com.hana8.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +14,7 @@ public class HelloController {
 	public String index() {
 		return "hana8 Demo";
 	}
+
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello, World!";
